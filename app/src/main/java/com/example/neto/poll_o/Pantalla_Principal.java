bd0a940e47802716d5eab891296d11b0c48a9a55
NetoEspeciano <especiano94@gmail.com>
2016-05-18 17:18:18 -0600

    prueba 1 para ver si funciona

--- app/src/main/java/com/example/neto/poll_o/Pantalla_Principal.java
+++ app/src/main/java/com/example/neto/poll_o/Pantalla_Principal.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Pantalla_Principal extends AppCompatActivity {
-
+    int pruebaGitHub;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
